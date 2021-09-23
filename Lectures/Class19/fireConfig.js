a49307258c4fcab952e57e58c1fd26d1aad5a6c5
pankkap <pankkap@gmail.com>
2021-09-23 11:38:41 +0530

    Update folder structure

---
+++ Lectures/Class19/fireConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDWLHLx6m1nV6pyqd8k5BFlWIsROQ4v4TI",
+  authDomain: "myfireprowithcrud.firebaseapp.com",
+  databaseURL: "https://myfireprowithcrud-default-rtdb.firebaseio.com",
+  projectId: "myfireprowithcrud",
+  storageBucket: "myfireprowithcrud.appspot.com",
+  messagingSenderId: "260569317938",
+  appId: "1:260569317938:web:8c1b27b561f9ea129cf22b",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
